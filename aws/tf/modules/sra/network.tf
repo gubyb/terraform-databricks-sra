1aee351496d0094469002bad61dea02cbb0630fe
Gustav <gustavbyberg91@gmail.com>
2025-06-18 12:37:21 +0200

    Remove internet

--- aws/tf/modules/sra/network.tf
+++ aws/tf/modules/sra/network.tf
@@ -14,9 +14,9 @@ module "vpc" {
   azs  = var.availability_zones
 
   enable_dns_hostnames   = true
-  enable_nat_gateway     = true
+  enable_nat_gateway     = false
   single_nat_gateway     = false
-  one_nat_gateway_per_az = true
+  one_nat_gateway_per_az = false
   create_igw             = false
 
   public_subnet_names = [for az in var.availability_zones : format("%s-public-%s", var.resource_prefix, az)]
